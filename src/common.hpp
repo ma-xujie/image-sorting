f199f8178c71ff34ec2c321dd5ced804a7ffba9a
ma-xujie <maxujie14@gmail.com>
2016-06-25 09:09:53 +0800

    add common.hpp

---
+++ src/common.hpp
@@ -0,0 +1,14 @@
+#ifndef COMMON_HPP_INCLUDE
+#define COMMON_HPP_INCLUDE
+
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <string>
+#include <vector>
+
+using namespace std;
+using namespace cv;
+
+#endif /* end of include guard: COMMON_HPP_INCLUDE */
